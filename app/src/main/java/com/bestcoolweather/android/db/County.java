4d17efb4e0ee2135c305e99eb03e51132ffc2680
panda <panda@gmail.com>
2018-04-13 20:58:33 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/bestcoolweather/android/db/County.java
@@ -0,0 +1,46 @@
+package com.bestcoolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by zxit on 2018/4/13.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int countyCode;
+    private int cityId;
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(int countyCode) {
+        this.countyCode = countyCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
